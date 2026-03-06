ec2765ef175746f4992619e8693c4399e201f1d7
Bonga Mokete <bmokete@tracker.co.za>
2026-03-06 19:24:09 +0200

    implement header block

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderBlockComponent } from './design-system/components/header-block/header-block.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HeaderBlockComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'email-template-builder';
-}
+export class AppComponent {}
--- src/app/design-system/components/header-block/header-block.component.ts
+++ src/app/design-system/components/header-block/header-block.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { NgIf } from '@angular/common';
 
 @Component({
-  selector: 'app-header-block',
+  selector: 'header-block',
   standalone: true,
-  imports: [],
   templateUrl: './header-block.component.html',
   styleUrl: './header-block.component.scss'
 })
 export class HeaderBlockComponent {
-
+  @Input() title: string = '';
+  @Input() logoUrl?: string;
+  @Input() bgColor: any;
 }
