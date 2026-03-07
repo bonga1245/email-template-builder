1dfd83467386a73eea0d0db9d680680f686521af
Bonga Mokete <bmokete@tracker.co.za>
2026-03-07 09:04:24 +0200

    Add data model

---
+++ src/app/models/template-block.model.ts
@@ -0,0 +1,35 @@
+export type BlockType = 'header' | 'text' | 'image' | 'button';
+
+export interface HeaderBlockData {
+  type: 'header';
+  id: string;
+  title: string;
+  logoUrl?: string;
+  bgColor?: string;
+}
+
+export interface TextBlockData {
+  type: 'text';
+  id: string;
+  content: string;
+  alignment: 'left' | 'center' | 'right';
+}
+
+export interface ImageBlockData {
+  type: 'image';
+  id: string;
+  src: string;
+  alt: string;
+  width?: string;
+}
+
+export interface ButtonBlockData {
+  type: 'button';
+  id: string;
+  label: string;
+  href: string;
+  align: 'left' | 'center' | 'right';
+  bgColor?: string;
+}
+
+export type TemplateBlock = HeaderBlockData | TextBlockData | ImageBlockData | ButtonBlockData;
